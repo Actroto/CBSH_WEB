92315172ab8ba12199328ded51859b6b30c033d0
Actroto <102223361+Actroto@users.noreply.github.com>
2022-05-13 08:38:55 +0900

    Delete sport-day directory

--- sport-day/sport_event_list.js
+++
@@ -1,8 +0,0 @@
-// list item mean [grade, classNumber]
-const class_list = [[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2]];
-// Both lists must be the same length.
-const sport_event_list_hangul = ["축구","족구","배구","피구","배드민턴","줄다리기","미션 이어달리기","계주"];
-const sport_event_list = ["soccer","foot-volleyball","volleyball","dodge-ball","badminton","tug-of-war","mission-relay","relay"];
-
-
-// It takes about 8minutes for hosting server(github.io) to apply the changes of lists. (2022-04-08)
